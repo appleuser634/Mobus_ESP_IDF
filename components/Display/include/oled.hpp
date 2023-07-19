69bb5ac9e56dff32c93ff5e0f361a0ca91c02abc
mimoc <mimoc@mimocnoiMac.elecom>
2023-07-20 00:06:23 +0900

    fix: display config

--- components/Display/include/oled.hpp
+++ components/Display/include/oled.hpp
@@ -11,7 +11,7 @@
 #pragma once
 
 class LGFX : public lgfx::LGFX_Device {
-    lgfx::Panel_SSD1306 _panel_instance;
+    lgfx::Panel_SH110x _panel_instance;
     lgfx::Bus_I2C _bus_instance;
 
    public:
@@ -44,7 +44,7 @@ class LGFX : public lgfx::LGFX_Device {
             cfg.memory_height = 64;  // ドライバICがサポートしている最大の高さ
             cfg.panel_width = 128;  // 実際に表示可能な幅
             cfg.panel_height = 64;  // 実際に表示可能な高さ
-            cfg.offset_x = 0;       // パネルのX方向オフセット量
+            cfg.offset_x = 2;       // パネルのX方向オフセット量
             cfg.offset_y = 0;       // パネルのY方向オフセット量
             cfg.offset_rotation =
                 0;  // 回転方向の値のオフセット 0~7 (4~7は上下反転)
