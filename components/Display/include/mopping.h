d55b5ad923c1019810c7cac4f888cfcf5a3b7d70
appleuser634 <appleuser634>
2025-07-04 12:13:01 +0900

    add: Moppingのメインゲームを移植

--- components/Display/include/mopping.h
+++ components/Display/include/mopping.h
@@ -80,6 +80,7 @@ void opening() {
     Button type_button(GPIO_NUM_46);
     Button back_button(GPIO_NUM_3);
     Button enter_button(GPIO_NUM_5);
+
     sprite.setFont(&fonts::Font2);
 
     for (int i = 0; i < 20; i += 1) {
@@ -240,214 +241,244 @@ void opening() {
     }
 }
 
-// void show_clear() {
-//     lcd.fillScreen(0x000000u);
-//     ssd1306_drawImage(40, 0, clear, 40, 64, 0);
-//     sprite.pushSprite(&lcd, 0, 0);
-// }
-//
-// void gen_enemy(character *enemy) {
-//     int character_n = get_random(10);
-//     if (character_n <= 5) {
-//         enemy[2].id = 0;
-//         enemy[2].img = grass_1;
-//         enemy[2].img_1 = grass_1;
-//         enemy[2].img_2 = grass_1;
-//         enemy[2].show = true;
-//         enemy[2].y = 53;
-//         enemy[2].w = 8;
-//         enemy[2].h = 8;
-//     } else if (character_n <= 8) {
-//         enemy[2].id = 1;
-//         enemy[2].img = pineapple_1;
-//         enemy[2].img_1 = pineapple_1;
-//         enemy[2].img_2 = pineapple_1;
-//         enemy[2].show = true;
-//         enemy[2].y = 45;
-//         enemy[2].w = 16;
-//         enemy[2].h = 16;
-//     } else if (character_n <= 9) {
-//         enemy[2].id = 2;
-//         enemy[2].img = mongoose_1;
-//         enemy[2].img_1 = mongoose_1;
-//         enemy[2].img_2 = mongoose_2;
-//         enemy[2].show = true;
-//         enemy[2].y = 53;
-//         enemy[2].w = 16;
-//         enemy[2].h = 8;
-//     } else if (character_n <= 10) {
-//         enemy[2].id = 3;
-//         enemy[2].img = earthworm_1;
-//         enemy[2].img_1 = earthworm_1;
-//         enemy[2].img_2 = earthworm_2;
-//         enemy[2].show = true;
-//         enemy[2].y = 53;
-//         enemy[2].w = 8;
-//         enemy[2].h = 8;
-//     }
-//
-//     int enemy_distance = get_random(30);
-//     int distance_buffer = 60;
-//     enemy[2].x = enemy[1].x + distance_buffer + enemy_distance;
-// }
-//
-// bool game_loop() {
-//     int init_kuina_x = 15;
-//     int init_kuina_y = 45;
-//     character kuina = {100,          kuina_1,      kuina_1, kuina_2, 0,
-//                        init_kuina_x, init_kuina_y, 16,      16};
-//
-//     // struct character enemy =
-//     // {pineapple_1,pineapple_1,pineapple_1,130,53,8,8};
-//     character enemy[3] = {
-//         {0, grass_1, grass_1, grass_1, 1, 130, 53, 8, 8},
-//         {0, grass_1, grass_1, grass_1, 1, 160, 53, 8, 8},
-//         {1, pineapple_1, pineapple_1, pineapple_1, 1, 210, 45, 16, 16}};
-//
-//     int flip_c = 0;
-//     bool flip_flag = false;
-//
-//     bool button_state = false;
-//
-//     bool jump_flag = false;
-//     int max_jump = 30;
-//     int jump_progress = 0;
-//
-//     bool danger_flag = false;
-//
-//     // 0 is grass      50%
-//     // 1 is pineapple  30%
-//     // 2 is mongoose   10%
-//     // 3 is earthworm  10%
-//     int enemy_num = 0;
-//
-//     int random_n = get_random(10);
-//
-//     int count = 0;
-//     int score = 0;
-//
-//     bool loop_flag = true;
-//
-//     bool hart_flag = false;
-//     int hart_cnt = 0;
-//
-//     while (loop_flag) {
-//         uint8_t button_is_pressed = !GPIO_digitalRead(BTN_PIN);
-//
-//         // jump trigger
-//         if (!button_state && button_is_pressed && !jump_flag) {
-//             jump_flag = true;
-//         }
-//         button_state = button_is_pressed;
-//
-//         // jump animation
-//         if (jump_flag && (max_jump * 2) < jump_progress) {
-//             jump_progress = 0;
-//             jump_flag = false;
-//             kuina.y = init_kuina_y;
-//         } else if (jump_flag && max_jump >= jump_progress) {
-//             kuina.y -= 1;
-//             jump_progress += 1;
-//         } else if (jump_flag && max_jump < jump_progress) {
-//             kuina.y += 1;
-//             jump_progress += 1;
-//         }
-//
-//         flip_c++;
-//         if (flip_c > 20) {
-//             flip_flag = !flip_flag;
-//             flip_c = 0;
-//         }
-//
-//         if (enemy[0].x < 0) {
-//             enemy[0] = enemy[1];
-//             enemy[1] = enemy[2];
-//             gen_enemy(enemy);
-//         }
-//
-//         // flip character
-//         if (flip_flag) {
-//             enemy[0].img = enemy[0].img_1;
-//             enemy[1].img = enemy[1].img_1;
-//             enemy[2].img = enemy[2].img_1;
-//             kuina.img = kuina.img_1;
-//         } else {
-//             enemy[0].img = enemy[0].img_2;
-//             enemy[1].img = enemy[1].img_2;
-//             enemy[2].img = enemy[2].img_2;
-//             kuina.img = kuina.img_2;
-//         }
-//
-//         for (int i = 0; i < 3; i++) {
-//             // draw enemy
-//             if (enemy[i].show) {
-//                 ssd1306_drawImage(enemy[i].x, enemy[i].y, enemy[i].img,
-//                                   enemy[i].w, enemy[i].h, 0);
-//                 // ssd1306_drawRect(enemy[i].x, enemy[i].y, enemy[i].w,
-//                 // enemy[i].h, 1);
-//             }
-//
-//             enemy[i].x -= 1;
-//
-//             // hit judge
-//             if (kuina.x + 12 < enemy[i].x + 12 &&
-//                 (kuina.x + 4 + kuina.w) > enemy[i].x + 12 &&
-//                 kuina.y > (init_kuina_y - enemy[i].h)) {
-//                 if (enemy[i].id == 1 | enemy[i].id == 2) {
-//                     ssd1306_drawstr_sz(25, 20, "GAME OVER!!", 1,
-//                     fontsize_8x8); loop_flag = false; break;
-//                 } else if (enemy[i].id == 3 && enemy[i].show) {
-//                     // bonus point
-//                     score += 100;
-//                     enemy[i].show = false;
-//                     hart_flag = true;
-//                 }
-//             }
-//         }
-//
-//         if (hart_flag && hart_cnt < 60) {
-//             ssd1306_drawImage(20, 30, hart, 8, 8, 0);
-//             hart_cnt++;
-//         } else {
-//             hart_flag = false;
-//             hart_cnt = 0;
-//         }
-//
-//         // draw kuina
-//         ssd1306_drawImage(kuina.x, kuina.y, kuina.img, kuina.w, kuina.h, 0);
-//
-//         count += 1;
-//         if (count > 10) {
-//             score += 1;
-//             count = 0;
-//         }
-//
-//         // draw jump score
-//         char score_txt[30] = "Score:";
-//         char str_score[30];
-//         intToStr(score, str_score);
-//         string_concat(score_txt, str_score);
-//         ssd1306_drawstr_sz(0, 0, score_txt, 1, fontsize_8x8);
-//
-//         // draw_road();
-//         sprite.pushSprite(&lcd, 0, 0);
-//         lcd.fillScreen(0x000000u);
-//     }
-//
-//     vTaskDelay(1000/portTICK_PERIOD_MS);
-//     // wait push button...
-//     while (1) {
-//         uint8_t button_is_pressed = !GPIO_digitalRead(BTN_PIN);
-//         // jump trigger
-//         if (!button_state && button_is_pressed) {
-//             break;
-//         }
-//         button_state = button_is_pressed;
-//     }
-// }
+void gen_enemy(character *enemy) {
+    int character_n = get_random(10);
+    if (character_n <= 5) {
+        enemy[2].id = 0;
+        enemy[2].img = grass_1;
+        enemy[2].img_1 = grass_1;
+        enemy[2].img_2 = grass_1;
+        enemy[2].show = true;
+        enemy[2].y = 53;
+        enemy[2].w = 8;
+        enemy[2].h = 8;
+    } else if (character_n <= 8) {
+        enemy[2].id = 1;
+        enemy[2].img = pineapple_1;
+        enemy[2].img_1 = pineapple_1;
+        enemy[2].img_2 = pineapple_1;
+        enemy[2].show = true;
+        enemy[2].y = 45;
+        enemy[2].w = 16;
+        enemy[2].h = 16;
+    } else if (character_n <= 9) {
+        enemy[2].id = 2;
+        enemy[2].img = mongoose_left_1;
+        enemy[2].img_1 = mongoose_left_1;
+        enemy[2].img_2 = mongoose_left_2;
+        enemy[2].show = true;
+        enemy[2].y = 53;
+        enemy[2].w = 16;
+        enemy[2].h = 8;
+    } else if (character_n <= 10) {
+        enemy[2].id = 3;
+        enemy[2].img = earthworm_1;
+        enemy[2].img_1 = earthworm_1;
+        enemy[2].img_2 = earthworm_2;
+        enemy[2].show = true;
+        enemy[2].y = 53;
+        enemy[2].w = 8;
+        enemy[2].h = 8;
+    }
+
+    int enemy_distance = get_random(30);
+    int distance_buffer = 60;
+    enemy[2].x = enemy[1].x + distance_buffer + enemy_distance;
+}
+
+bool game_loop() {
+    int init_kuina_x = 15;
+    int init_kuina_y = 45;
+    character kuina = {100,          kuina_1,      kuina_1, kuina_2, 0,
+                       init_kuina_x, init_kuina_y, 16,      16};
+
+    // struct character enemy =
+    // {pineapple_1,pineapple_1,pineapple_1,130,53,8,8};
+    character enemy[3] = {
+        {0, grass_1, grass_1, grass_1, 1, 130, 53, 8, 8},
+        {0, grass_1, grass_1, grass_1, 1, 160, 53, 8, 8},
+        {1, pineapple_1, pineapple_1, pineapple_1, 1, 210, 45, 16, 16}};
+
+    int flip_c = 0;
+    bool flip_flag = false;
+
+    bool button_state = false;
+
+    bool jump_flag = false;
+    int max_jump = 30;
+    int jump_progress = 0;
+
+    bool danger_flag = false;
+
+    // 0 is grass      50%
+    // 1 is pineapple  30%
+    // 2 is mongoose   10%
+    // 3 is earthworm  10%
+    int enemy_num = 0;
+
+    int random_n = get_random(10);
+
+    int count = 0;
+    int score = 0;
+
+    bool loop_flag = true;
+
+    bool hart_flag = false;
+    int hart_cnt = 0;
+
+    Joystick joystick;
+
+    Button type_button(GPIO_NUM_46);
+    Button back_button(GPIO_NUM_3);
+    Button enter_button(GPIO_NUM_5);
+
+    while (loop_flag) {
+        // Joystickの状態を取得
+        Joystick::joystick_state_t joystick_state =
+            joystick.get_joystick_state();
+
+        // スイッチの状態を取得
+        Button::button_state_t type_button_state =
+            type_button.get_button_state();
+        Button::button_state_t back_button_state =
+            back_button.get_button_state();
+        Button::button_state_t enter_button_state =
+            enter_button.get_button_state();
+
+        // jump trigger
+        if (type_button_state.pushed && !jump_flag) {
+            jump_flag = true;
+            type_button.clear_button_state();
+            type_button.reset_timer();
+            joystick.reset_timer();
+        }
+
+        // jump animation
+        if (jump_flag && (max_jump * 2) < jump_progress) {
+            jump_progress = 0;
+            jump_flag = false;
+            kuina.y = init_kuina_y;
+        } else if (jump_flag && max_jump >= jump_progress) {
+            kuina.y -= 1;
+            jump_progress += 1;
+        } else if (jump_flag && max_jump < jump_progress) {
+            kuina.y += 1;
+            jump_progress += 1;
+        }
+
+        flip_c++;
+        if (flip_c > 20) {
+            flip_flag = !flip_flag;
+            flip_c = 0;
+        }
+
+        if (enemy[0].x < 0) {
+            enemy[0] = enemy[1];
+            enemy[1] = enemy[2];
+            gen_enemy(enemy);
+        }
+
+        // flip character
+        if (flip_flag) {
+            enemy[0].img = enemy[0].img_1;
+            enemy[1].img = enemy[1].img_1;
+            enemy[2].img = enemy[2].img_1;
+            kuina.img = kuina.img_1;
+        } else {
+            enemy[0].img = enemy[0].img_2;
+            enemy[1].img = enemy[1].img_2;
+            enemy[2].img = enemy[2].img_2;
+            kuina.img = kuina.img_2;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            // draw enemy
+            if (enemy[i].show) {
+                sprite.drawBitmap(enemy[i].x, enemy[i].y, enemy[i].img,
+                                  enemy[i].w, enemy[i].h, TFT_WHITE, TFT_BLACK);
+                // ssd1306_drawRect(enemy[i].x, enemy[i].y, enemy[i].w,
+                // enemy[i].h, 1);
+            }
+
+            enemy[i].x -= 1;
+
+            // hit judge
+            if (kuina.x + 12 < enemy[i].x + 12 &&
+                (kuina.x + 4 + kuina.w) > enemy[i].x + 12 &&
+                kuina.y > (init_kuina_y - enemy[i].h)) {
+                if (enemy[i].id == 1 || enemy[i].id == 2) {
+                    sprite.setCursor(25, 20);
+                    sprite.print("GAME OVER!!");
+                    loop_flag = false;
+                    break;
+                } else if (enemy[i].id == 3 && enemy[i].show) {
+                    // bonus point
+                    score += 100;
+                    enemy[i].show = false;
+                    hart_flag = true;
+                }
+            }
+        }
+
+        if (hart_flag && hart_cnt < 60) {
+            sprite.drawBitmap(30, 30, hart, 8, 8, TFT_WHITE, TFT_BLACK);
+            hart_cnt++;
+        } else {
+            hart_flag = false;
+            hart_cnt = 0;
+        }
+
+        // draw kuina
+        sprite.drawBitmap(kuina.x, kuina.y, kuina.img, kuina.w, kuina.h,
+                          TFT_WHITE, TFT_BLACK);
+
+        count += 1;
+        if (count > 10) {
+            score += 1;
+            count = 0;
+        }
+
+        // draw jump score
+        char score_txt[30] = "Score:";
+        char str_score[30];
+        intToStr(score, str_score);
+        string_concat(score_txt, str_score);
+        sprite.setCursor(0, 0);
+        sprite.print(score_txt);
+
+        // draw_road();
+        sprite.pushSprite(&lcd, 0, 0);
+        sprite.fillRect(0, 0, 128, 64, 0);
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // wait push button...
+    while (1) {
+        // Joystickの状態を取得
+        Joystick::joystick_state_t joystick_state =
+            joystick.get_joystick_state();
+
+        // スイッチの状態を取得
+        Button::button_state_t type_button_state =
+            type_button.get_button_state();
+        Button::button_state_t back_button_state =
+            back_button.get_button_state();
+        Button::button_state_t enter_button_state =
+            enter_button.get_button_state();
+        // jump trigger
+        if (type_button_state.pushed) {
+            type_button.clear_button_state();
+            type_button.reset_timer();
+            joystick.reset_timer();
+            break;
+        }
+    }
+    return true;
+}
 
 void mopping_main() {
     opening();
+    game_loop();
 
     // clear display
     // lcd.fillScreen(0x000000u);
